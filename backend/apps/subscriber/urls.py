9a2e6e698f2b4ae1228582eaec2434d939e32204
se <dyhwang@stealien.com>
2026-06-12 16:09:44 +0900

    feat: 고객사용 구독 링크 발행 기능 추가
    - CustomerSubscriptionToken 모델: 고객사별 UUID 토큰 + 만료일시 관리
    - Subscribers 관리 페이지: 링크 발행/재발행/취소 모달, 만료 D-Day 표시
    - 공개 구독 페이지 (/subscriber/subscribe/{token}/): 로그인 불필요, 솔루션별 Gmail 구독 토글, 수신 이메일 추가·삭제

--- backend/apps/subscriber/urls.py
+++ backend/apps/subscriber/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
-from .views import SubscriberManagementView, get_customer_subscriptions, save_customer_subscription
+from .views import (
+    SubscriberManagementView,
+    get_customer_subscriptions,
+    save_customer_subscription,
+    issue_token,
+    revoke_token,
+    subscribe_page,
+    subscribe_toggle_solution,
+    subscribe_add_email,
+    subscribe_remove_email,
+)
 
 app_name = 'subscriber'
 
@@ -7,4 +17,10 @@ urlpatterns = [
     path('', SubscriberManagementView.as_view(), name='subscriber_management'),
     path('subscriptions/<int:customer_id>/', get_customer_subscriptions, name='get_subscriptions'),
     path('save/', save_customer_subscription, name='save_subscription'),
+    path('token/issue/<int:customer_id>/', issue_token, name='issue_token'),
+    path('token/revoke/<int:customer_id>/', revoke_token, name='revoke_token'),
+    path('subscribe/<uuid:token>/', subscribe_page, name='subscribe_page'),
+    path('subscribe/<uuid:token>/toggle-solution/', subscribe_toggle_solution, name='subscribe_toggle_solution'),
+    path('subscribe/<uuid:token>/add-email/', subscribe_add_email, name='subscribe_add_email'),
+    path('subscribe/<uuid:token>/remove-email/', subscribe_remove_email, name='subscribe_remove_email'),
 ]
