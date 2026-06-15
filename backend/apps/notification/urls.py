de3df42a79fc25ffff82c8acc5eaa0d2a997d891
se <dyhwang@stealien.com>
2026-06-15 09:33:57 +0900

    feat: Notice Settings에 패치노트 이메일 미리보기 탭 추가
    공문/패치노트 토글 버튼으로 두 가지 이메일 형식 미리보기 전환
    패치노트 미리보기는 샘플 데이터로 렌더링

--- backend/apps/notification/urls.py
+++ backend/apps/notification/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import OfficialNoticeView, NoticeConfigView, get_recipients_preview, send_notice, preview_email, view_notice
+from .views import OfficialNoticeView, NoticeConfigView, get_recipients_preview, send_notice, preview_email, preview_patchnote_email, view_notice
 
 app_name = 'notification'
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('recipients_preview/', get_recipients_preview, name='recipients_preview'),
     path('send/', send_notice, name='send_notice'),
     path('preview/', preview_email, name='preview_email'),
+    path('preview/patchnote/', preview_patchnote_email, name='preview_patchnote_email'),
     path('notice/<int:notice_id>/', view_notice, name='view_notice'),
 ]
